96d89e6cc936ff5bd882ec23cb3ed00c1ec9f790
vladas9 <vladamza63@gmail.com>
2024-09-11 15:26:56 +0300

    feat server: add init controller function in newServer

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 	"reflect"
 
-	"github.com/vladas9/backend-practice/internal/controllers"
+	c "github.com/vladas9/backend-practice/internal/controllers"
 	u "github.com/vladas9/backend-practice/internal/utils"
-	"github.com/vladas9/controllers"
+	p "github.com/vladas9/backend-practice/pkg/postgres"
 )
 
-type apiFunc func(w http.ResponseWriter, r *http.Request) *controllers.ApiError
+type apiFunc func(w http.ResponseWriter, r *http.Request) *c.ApiError
 
 func (fn apiFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if apiErr := fn(w, r); apiErr != nil {
@@ -19,41 +19,51 @@ func (fn apiFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type UnitWorker interface{} // dummy temp type, delete later
 type Server struct {
-	ListenAddr string
-	Router     *http.ServeMux
-	UnitWorker UnitWorker
+	ListenAddr  string
+	Router      *http.ServeMux
+	Controllers *c.Controller
 }
 
 func NewServer(addr string) *Server {
-
+	db, err := p.ConnectDB()
+	if err != nil {
+		//TODO: Add error message if db dont open
+	}
 	mux := http.NewServeMux()
+
+	controller, err := c.NewController(db)
+	if err != nil {
+		//TODO: Add error message if creating controller fails
+	}
+
 	return &Server{
-		ListenAddr: addr,
-		Router:     mux,
+		ListenAddr:  addr,
+		Router:      mux,
+		Controllers: controller,
 	}
 }
 
 func (s *Server) Run() {
 	//TODO: Add controllers links
-	s.Router.Handle("POST /api/users/register", apiFunc(controllers.SignUp))
-	s.Router.Handle("POST /api/users/login", apiFunc(controllers.SignIn))
-	s.Router.Handle("GET /api/users/{id}", apiFunc(controllers.))
-	s.Router.Handle("PUT /api/users/{id}", apiFunc(controllers.))
-	s.Router.Handle("GET /api/items", apiFunc(controllers.))
-	s.Router.Handle("GET /api/items/{id}", apiFunc(controllers.))
-	s.Router.Handle("POST /api/items", apiFunc(controllers.))
-	s.Router.Handle("GET /api/auctions", apiFunc(controllers.))
-	s.Router.Handle("POST /api/auctions", apiFunc(controllers.))
-	s.Router.Handle("GET /api/auctions/{id}", apiFunc(controllers.))
-	s.Router.Handle("PUT /api/auction/{id}/bid", apiFunc(controllers.))
-	s.Router.Handle("", apiFunc(controllers.)) //TODO: WebSocket end point
-	s.Router.Handle("POST /api/bids", apiFunc(controllers.))
-	s.Router.Handle("POST /api/payments", apiFunc(controllers.))
-	s.Router.Handle("GET /api/notifications", apiFunc(controllers.))
-	s.Router.Handle("PUT /api/notifications/{id}/read", apiFunc(controllers.))
-	u.Logger.Info("Registered Routes")
+
+	s.Router.Handle("POST /api/users/register", apiFunc(s.Controllers.Register))
+	// s.Router.Handle("POST /api/users/login", apiFunc(controllers.SignIn))
+	// s.Router.Handle("GET /api/users/{id}", apiFunc(controllers.))
+	// s.Router.Handle("PUT /api/users/{id}", apiFunc(controllers.))
+	// s.Router.Handle("GET /api/items", apiFunc(controllers.))
+	// s.Router.Handle("GET /api/items/{id}", apiFunc(controllers.))
+	// s.Router.Handle("POST /api/items", apiFunc(controllers.))
+	// s.Router.Handle("GET /api/auctions", apiFunc(controllers.))
+	// s.Router.Handle("POST /api/auctions", apiFunc(controllers.))
+	// s.Router.Handle("GET /api/auctions/{id}", apiFunc(controllers.))
+	// s.Router.Handle("PUT /api/auction/{id}/bid", apiFunc(controllers.))
+	// s.Router.Handle("", apiFunc(controllers.)) //TODO: WebSocket end point
+	// s.Router.Handle("POST /api/bids", apiFunc(controllers.))
+	// s.Router.Handle("POST /api/payments", apiFunc(controllers.))
+	// s.Router.Handle("GET /api/notifications", apiFunc(controllers.))
+	// s.Router.Handle("PUT /api/notifications/{id}/read", apiFunc(controllers.))
+	// u.Logger.Info("Registered Routes")
 
 	u.Logger.Info("Started server on", s.ListenAddr)
 	u.Logger.Error(http.ListenAndServe(s.ListenAddr, s.Router))
