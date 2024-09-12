827113467ddbd73423462eee6b6b804ba935e098
Andre Chicu <andriesh.chicu@gmail.com>
2024-09-12 20:24:58 +0300

    decimal type fix

--- internal/models/item.go
+++ internal/models/item.go
@@ -2,14 +2,15 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 )
 
 type ItemModel struct {
-	ItemId        uuid.UUID   `json:"item_id"`
-	Name          string      `json:"name"`
-	Description   string      `json:"description"`
-	StartingPrice Decimal     `json:"starting_price"`
-	Category      Category    `json:"category"`
-	Condition     Condition   `json:"condition"`
-	Images        []uuid.UUID `json:"images"`
+	ItemId        uuid.UUID       `json:"item_id"`
+	Name          string          `json:"name"`
+	Description   string          `json:"description"`
+	StartingPrice decimal.Decimal `json:"starting_price"`
+	Category      Category        `json:"category"`
+	Condition     Condition       `json:"condition"`
+	Images        []uuid.UUID     `json:"images"`
 }
--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -1,15 +1,17 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 )
 
 type TransactionModel struct {
 	BaseModel
-	AuctionId       uuid.UUID `json:"auction_id"`
-	BuyerId         uuid.UUID `json:"buyer_id"`
-	SellerId        uuid.UUID `json:"seller_id"`
-	Amount          Decimal   `json:"amount"`
-	TransactionDate time.Time `json:"transaction_date"`
+	AuctionId       uuid.UUID       `json:"auction_id"`
+	BuyerId         uuid.UUID       `json:"buyer_id"`
+	SellerId        uuid.UUID       `json:"seller_id"`
+	Amount          decimal.Decimal `json:"amount"`
+	TransactionDate time.Time       `json:"transaction_date"`
 }
