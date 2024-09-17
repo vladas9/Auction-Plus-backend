ffe52427e9e93651901fd7bf85693f81593048a6
Andre Chicu <andriesh.chicu@gmail.com>
2024-09-17 12:26:46 +0300

    dummydata update

--- internal/utils/dummydata.go
+++ internal/utils/dummydata.go
@@ -8,43 +8,43 @@ import (
 	m "github.com/vladas9/backend-practice/internal/models"
 )
 
-func GenerateDummyAuctions() []*m.AuctionModel {
+func GenerateDummyAuctions(sellerId uuid.UUID) []*m.AuctionModel {
 	auctions := []*m.AuctionModel{
 		{
 			BaseModel:          m.BaseModel{uuid.New()},
-			SellerId:           uuid.New(),
-			StartingBid:        decimal.NewFromInt(100),
-			ClosingBid:         decimal.NewFromInt(200),
+			SellerId:           sellerId,
+			StartPrice:         decimal.NewFromInt(100),
+			CurrentBid:         decimal.NewFromInt(200),
 			StartTime:          time.Now().Add(-24 * time.Hour),
 			EndTime:            time.Now().Add(24 * time.Hour),
 			ExtraTimeDuration:  5 * time.Minute,
 			ExtraTimeThreshold: 10 * time.Minute,
 			ExtraTimeEnabled:   true,
-			IsActive:           true,
+			Status:             true,
 		},
 		{
 			BaseModel:          m.BaseModel{uuid.New()},
-			SellerId:           uuid.New(),
-			StartingBid:        decimal.NewFromInt(50),
-			ClosingBid:         decimal.NewFromInt(150),
+			SellerId:           sellerId,
+			StartPrice:         decimal.NewFromInt(50),
+			CurrentBid:         decimal.NewFromInt(150),
 			StartTime:          time.Now().Add(-48 * time.Hour),
 			EndTime:            time.Now().Add(48 * time.Hour),
 			ExtraTimeDuration:  10 * time.Minute,
 			ExtraTimeThreshold: 15 * time.Minute,
 			ExtraTimeEnabled:   false,
-			IsActive:           false,
+			Status:             false,
 		},
 		{
 			BaseModel:          m.BaseModel{uuid.New()},
-			SellerId:           uuid.New(),
-			StartingBid:        decimal.NewFromInt(300),
-			ClosingBid:         decimal.NewFromInt(350),
+			SellerId:           sellerId,
+			StartPrice:         decimal.NewFromInt(300),
+			CurrentBid:         decimal.NewFromInt(350),
 			StartTime:          time.Now().Add(-72 * time.Hour),
 			EndTime:            time.Now().Add(72 * time.Hour),
 			ExtraTimeDuration:  3 * time.Minute,
 			ExtraTimeThreshold: 5 * time.Minute,
 			ExtraTimeEnabled:   true,
-			IsActive:           true,
+			Status:             true,
 		},
 	}
 
@@ -53,9 +53,9 @@ func GenerateDummyAuctions() []*m.AuctionModel {
 
 func GenerateDummyUser() *m.UserModel {
 	return &m.UserModel{
-		BaseModel:      m.BaseModel{uuid.New()},
 		Username:       "john_doe",
 		Email:          "john.doe@example.com",
+		Image:          "",
 		Password:       "hashed_password", // In real case, this would be hashed.
 		Address:        "1234 Elm St, Springfield, USA",
 		PhoneNumber:    "+1-555-1234",
